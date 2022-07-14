2d8ef19ae6e2c8a9ccbf812c22282dcc73670c36
Cosmin - Stefan Negureanu <epsilon.number79@gmail.com>
2022-07-15 01:53:20 +0300

    added the feature to see, edit and update plugs

--- src/Controller/StationController.php
+++ src/Controller/StationController.php
@@ -3,8 +3,11 @@
 namespace App\Controller;
 
 
+use App\Entity\Plugs;
 use App\Entity\Station;
+use App\Form\PlugsType;
 use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
@@ -38,6 +41,65 @@ class StationController extends AbstractController
     public function redirection(): Response{
 
         return $this->redirectToRoute('app_stations', ['location' => $this->security->getUser()->getAddress()]);
+    }
+
+    #[Route('/stations/station={id}', name:'app_stations_details')]
+    public function details(int $id): Response{
+
+        $repository = $this->entityManager->getRepository(Station::class);
+        $repository2 = $this->entityManager->getRepository(Plugs::class);
+        $station = $repository->find($id);
+        $plugs = $repository2->findBy(['station' => $station->getId()]);
+
+
+        return $this->render('app/stations_detail.html.twig', [
+            'station' => $station,
+            'plugs' => $plugs
+        ]);
+
+    }
+
+    #[Route('/stations/station={id}/delete_plug={plugId}', name: 'app_station_delete_plug')]
+    public function deletePlug(int $id, int $plugId): Response{
+        $repository = $this->entityManager->getRepository(Plugs::class);
+
+        $plug = $repository->find($plugId);
+
+        $this->entityManager->remove($plug);
+
+        $this->entityManager->flush();
+        return $this->redirectToRoute('app_stations_details', [
+            'id' => $id
+        ]);
+    }
+
+    #[Route('/stations/station={id}/edit_plug={plugId}', name:'app_plugs_edit')]
+    public function updatePlug(int $id, int $plugId, Request $request): Response{
+        $repository = $this->entityManager->getRepository(Plugs::class);
+
+        $plug = $repository->find($plugId);
+
+        $station = $plug->getStation();
+        $form = $this->createForm(PlugsType::class, $plug);
+
+        $form->handleRequest($request);
+
+        if($form->isSubmitted() && $form->isValid()){
+            $plug->setType($form->get('type')->getData());
+            $plug->setStatus($form->get('status')->getData());
+
+            $this->entityManager->flush();
+
+            return $this->redirectToRoute('app_stations_details', [
+                'id' => $id
+            ]);
+        }
+
+        return $this->renderForm('app/plugs_edit.html.twig', [
+            'form' => $form,
+            'station' => $station
+        ]);
+
     }
 
 }
